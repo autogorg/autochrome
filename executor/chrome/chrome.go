6680a60e1a2c6e0de7968db8575c1dea2cbc5d0b
Chaoqun Han <74887456+hanchaoqun@users.noreply.github.com>
2024-03-27 14:56:42 +0300

    fix get html

--- executor/chrome/chrome.go
+++ executor/chrome/chrome.go
@@ -50,6 +50,8 @@ func (c *Chrome) SetUrl(url string) {
 func (c *Chrome) GetHtml() string {
 	c.RefreshContext()
 	chromedp.Run(c.Context,
+		// Wait document ready
+		chromedp.Evaluate(`document.readyState === "complete"`, nil),
 		// Read outerHTML
 		chromedp.OuterHTML("html", &c.Html),
 	)
